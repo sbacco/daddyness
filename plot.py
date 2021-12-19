92f6f2433f522186ae7743ce19324a0e201dae2d
Sergio Ricossa <sergio.ricossa@gmx.ch>
2021-12-19 16:09:31 +0100

    make plot script more flexible

--- plot.py
+++ plot.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-import matplotlib.pylab as plt
+from matplotlib import pyplot as plt
 import numpy as np
 
-x=np.linspace(0,20,2000)
+RANGE = 16
+PRECISION = 2 # decimal places
+
+x=np.linspace(0,RANGE,RANGE*10**PRECISION)
 f=np.log(9*(x+1))/np.log(2) + np.log(9*(x+1)/2)/np.log(10)
 # First time N crosses f(N)
 c = next(val for i, val in enumerate(x) if f[i] < x[i])
 
 plt.plot(x,x,label='$N$')
 plt.plot(x,f,label='$f(N)$')
-plt.axvline(x = c,linestyle=':', label='$N=$'+str(c))
+plt.axvline(x = c,linestyle=':', label=f'$N={c:.{PRECISION+1}}$')
 plt.xlabel('$N$')
 plt.legend()
 plt.show()
